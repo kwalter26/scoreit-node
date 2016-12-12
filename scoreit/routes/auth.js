c451b1a3b456ca1d75e0c39b10aefac5bfc01098
Kyle Walter <walter.kl26@gmail.com>
2016-12-11 23:15:58 -0600

    Adding express, database, and session configuration

---
+++ scoreit/routes/auth.js
@@ -0,0 +1,14 @@
+﻿var router = require('express').Router();
+
+module.exports = function (app) {
+
+    router.get('/login', function (req, res, next) {
+        res.send('Login');
+    });
+
+    router.get('/register', function (req, res, next) {
+        res.send('Register');
+    });
+
+    return router;
+}
--- scoreit/server.js
+++ scoreit/server.js
@@ -1,8 +1,40 @@
 'use strict';
-var http = require('http');
-var port = process.env.port || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+//Dependencies
+var express = require('express');
+var app = express();
+var port = process.env.port || 1337;
+var bodyParser = require('body-parser');
+var sessions = require('express-session');
+var passport = require('passport');
+var mongoose = require('mongoose');
+
+
+
+// Database Connection
+mongoose.connect(process.env.MONGO_STRING, function (err) {
+    if (err) console.log('Mongoose:   Error occured!', err);
+    else console.log('Mongoose:   Connected');
+});
+var MongoStore = require('connect-mongo')(sessions);
+var sessionStore = new MongoStore({ mongooseConnection: mongoose.connection, clear_interval: 10000 });
+
+// Sessions
+app.use(sessions({
+    secret: process.env.SECRET,
+    store: sessionStore,
+
+
+});
+
+ 
+// Routing
+app.use('/auth', require('./routes/auth')(app));
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+
+app.listen(port, function () {
+    console.log('Example app listening on port 3000!');
+});
