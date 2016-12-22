e00ce9c777a22690bf99dd77912666582db1a85e
Kyle Walter <walter.kl26@gmail.com>
2016-12-22 10:21:55 -0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-curl -H "Content-Type: application/json" --data '{"source_type": "Branch", "source_name": "master"}' -X POST https://registry.hub.docker.com/u/kwalter26/scoreit-node/trigger/40d5e8eb-2ea9-4077-9fca-1cf0bdbfade6/
+curl -X POST https://registry.hub.docker.com/u/kwalter26/scoreit-node/trigger/40d5e8eb-2ea9-4077-9fca-1cf0bdbfade6/
