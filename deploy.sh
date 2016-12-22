19995c9d4f42b4c320ec7a0006cb1ca59b1bcfc3
Kyle Walter <walter.kl26@gmail.com>
2016-12-22 09:23:56 -0600

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+!#/bin/bash
+curl -H "Content-Type: application/json" --data '{"source_type": "Branch", "source_name": "staging"}' -X POST https://registry.hub.docker.com/u/kwalter26/scoreit-node/trigger/40d5e8eb-2ea9-4077-9fca-1cf0bdbfade6/
